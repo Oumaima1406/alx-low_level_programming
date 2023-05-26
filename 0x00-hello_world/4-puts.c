b2f8237a1424fb9b2a41bd3e8a2e7ed67e0d6afd
Oumaima <lmimoumaima@gmail.com>
2023-05-26 04:17:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /**
  * main - Entry point of the program
- *
- * puts("/(to avoid " to be read ) programming)
- * return: always 0 success
- */
+ * description: puts to print a string of characters
+ * Return: 0 if successful.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
