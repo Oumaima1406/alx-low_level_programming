48bd65d8d017d614e6965441254d0ee027256a47
Oumaima <lmimoumaima@gmail.com>
2023-05-28 20:38:05 -0700

    reset

---
+++ 0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function takes a pointer to an int as parameter
+ * and it changes the pointed value to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
