17c38d6aac22c28df1b5258271bc98e1116680b2
Oumaima <lmimoumaima@gmail.com>
2023-06-03 16:32:52 -0700

    alphabet -q an e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints all alphabets without the letters q and e.
+ *
+ * Return: Always 0 if Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
